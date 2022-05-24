d62d154834eff212c6192b91049e5d2306b8d309
mirzasabbirgithub <mirzaasabbir@gmail.com>
2022-05-25 01:55:42 +0600

    portfolio added

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -3,7 +3,24 @@ import React from 'react';
 const Portfolio = () => {
           return (
                     <div>
-                              <h1>This portfolio page</h1>
+                              <h1 className='text-5xl mb-5 text-rose-600'>My Portfolio</h1>
+
+                              <div>
+                                        <h1 className="text-3xl text-lime-700">Personal Information</h1>
+                                        <h1 className="text-2xl ">Name: Mirza Sabbir Ahmed</h1>
+                                        <h1 className="text-2xl">Email: mirzaasabbir@gmail.com</h1>
+                                        <h1 className="text-3xl text-blue-400">Educational Information</h1>
+                                        <p className="text-2xl">Fresh Graduate in BS CSE from North South University</p>
+                                        <h1 className="text-3xl text-green-600">Skills:</h1>
+                                        <p>Junior Web Developer</p>
+                                        <p>React Development</p>
+                                        <p>System Analyst</p>
+                                        <h1 className="text-3xl text-rose-600">Project Links</h1>
+                                        <p>https://comfy-gnome-584e96.netlify.app/</p>
+                                        <p>https://doctor-site-4144d.web.app/</p>
+                                        <p>https://books-warehouse-510d4.web.app/</p>
+
+                              </div>
                     </div>
           );
 };
